1ac38ef7b0f2686fbc56d3696d780142786c62c1
baronguo <906379252@qq.com>
2024-05-04 21:37:37 +0800

    add app name in application.yml add debug log config add eureka config

--- microservice-simple-provider-user/src/main/java/com/itmuch/cloud/ProviderUserApplication.java
+++ microservice-simple-provider-user/src/main/java/com/itmuch/cloud/ProviderUserApplication.java
@@ -2,11 +2,13 @@ package com.itmuch.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Hello world!
  *
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ProviderUserApplication{
     public static void main( String[] args ){
--- microservice-simple-provider-user/src/main/java/com/itmuch/cloud/controller/UserController.java
+++ microservice-simple-provider-user/src/main/java/com/itmuch/cloud/controller/UserController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * Created by BaronGuo on 2019/2/16.
  */
@@ -20,4 +25,30 @@ public class UserController {
         User user = this.userRepository.findOne(id);
         return user;
     }
+
+    @GetMapping("/allUsers")
+    public List<User> findAll(){
+        List<User> usersList = new ArrayList<>();
+        usersList = this.userRepository.findAll();
+        usersList.sort(new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return o1.getAge() - o2.getAge();
+            }
+        });
+        return usersList;
+    }
+
+    @GetMapping("/save")
+    public User saveAndFlush(User user){
+        //user.setId(new Long("4"));
+        user.setAge(25);
+        user.setBalance(new BigDecimal(5000));
+        user.setName("BARON");
+        user.setUsername("bg1131");
+        this.userRepository.saveAndFlush(user);
+        return user;
+    }
+
+
 }
